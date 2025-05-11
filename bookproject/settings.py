06df489b4c0d0568fa60bfc3f767179b9ee6f48e
akishino-007 <akishino1953@gmail.com>
2025-05-11 12:22:50 +0900

    forth

--- bookproject/settings.py
+++ bookproject/settings.py
@@ -133,9 +133,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = BASE_DIR / 'static'
+#if not DEBUG:
+#    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
